ed56725347fe85a8e220f909724e65399c5567c0
Tulio Filho <tuliofilho@outlook.com>
2022-03-25 12:59:50 -0300

    Texte environment

--- src/api/irregularVerbs.api.ts
+++ src/api/irregularVerbs.api.ts
@@ -1,8 +1,7 @@
-import { FetchHttp } from './request';
-import { Envs, Enums } from "../utils"
+import { FetchHttp } from "./request";
+import { Envs, Enums } from "../utils";
 
-const API_IRREGULAR_VERBS = `${Envs.REACT_APP_API_HOST}${Enums.API_PATH.IRREGULAR_VERBS}`;
+const API_IRREGULAR_VERBS = `${Envs.REACT_APP_API_HOST}/${Enums.API_PATH.IRREGULAR_VERBS}`;
 
-export const getIrregularVerbs =
-    async (): Promise<Array<IrregularVerb>> => 
-        await FetchHttp.get(API_IRREGULAR_VERBS);
+export const getIrregularVerbs = async (): Promise<Array<IrregularVerb>> =>
+  await FetchHttp.get(API_IRREGULAR_VERBS);
